dea107d12032d962d6c193bf0d4d19e074dd35d4
amac81 <arnaldocanelas@gmail.com>
2024-08-28 17:17:35 +0100

    SaleRepository update: reportSearch query

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
@@ -19,7 +20,6 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
        GROUP BY TB_SELLER.NAME 
 	 * 
 	 * */
-	
 		
 	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
 			+ "FROM Sale obj "
@@ -27,5 +27,18 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 			+ "GROUP BY obj.seller.name")
 	List<SaleSummaryDTO> summarySearch(LocalDate minDate, LocalDate maxDate);
 
+	
+	/*
+	 * SELECT TB_SALES.ID, TB_SALES.DATE, TB_SALES.AMOUNT, TB_SELLER.NAME AS sellerName
+	   FROM TB_SALES JOIN TB_SELLER ON (TB_SALES.SELLER_ID = TB_SELLER.ID) 
+       WHERE DATE >= '2022-05-01' AND DATE <= '2022-05-31'  AND TB_SELLER.NAME LIKE '%Odinson%'
+	 * 
+	 * */
+	
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name AS sellerName) "
+			+ "FROM Sale obj "
+			+ "WHERE obj.date BETWEEN (:minDate) AND obj.date (:maxDate) AND obj.seller.name LIKE UPPER(:name)")
+	List<SaleReportDTO> reportSearch(LocalDate minDate, LocalDate maxDate, String name);
+	
 }
 
